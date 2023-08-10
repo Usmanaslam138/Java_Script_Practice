1981747c1d4bb495529893a5793226336455d3f4
Usman Digitaldev <usmanaslamm138@gmail.com>
2023-08-10 20:46:49 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+// {
+//   var num1 = Number(prompt("Enter the first number: "));
+//   var num2 = Number(prompt("Enter the second number: "));
+//   var num3 = num1 + num2;
+//   var num4 = num1 * num2;
+//   var num5 = num1 / num2;
+// }
+// alert(
+//   "Addition is : " +
+//   num3 +
+//   "\n" +
+//   "Multiplication is : " +
+//   num4 +
+//   "\n" +
+//   "Division is : " +
+//   num5
+// );
+// num6 = num3 ** num4;
+// alert(num6);
+// {
+//   let num1 = prompt("Enter the first number: ");
+//   let num2 = prompt("Enter the second number: ");
+//   let num3 = num1 % num2;
+//   alert("The reminder is: " + num3);
+
+//   const num = 345434;
+//   console.log(num);
+// }
+
+let word = prompt("")
+// if (word == "usman") {
+//   alert('Welcome to my website')
+// } else if (word == "Hamza") {
+//   alert('welcome back Hamza');
+// } else {
+//   alert("you are not welcome");
+// }
+
+//switch case statement
+
+// switch (word) {
+//   case 'usman':
+//     alert(`Hello ${word}`)
+//     break;
+//   case 'hamza':
+//     alert(`${word}, Welcome Back`)
+//     break;
+//   default:
+//     alert('You are not allowed here')
+// }
+
